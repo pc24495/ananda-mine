3870ce43b0b024c6b5a5ad50c0611714ae875371
pc24495 <pchummar42@gmail.com>
2023-06-23 17:56:56 -0400

    Changed config file to query environment variables

--- client/src/config/axiosConfig.js
+++ client/src/config/axiosConfig.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://localhost:5173/api", // Replace with your desired base URL
+  baseURL: process.env.REACT_APP_PROD
+    ? "http://18.233.100.220:8080/api"
+    : "http://localhost:5173/api", // Replace with your desired base URL
 });
 
 export default instance;
