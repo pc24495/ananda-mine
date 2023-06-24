fe37f918f1a75ebd46c2d1307acf6fc0131ca1f7
pc24495 <pchummar42@gmail.com>
2023-06-24 13:58:58 -0400

    Fixed Cors configuration

--- anandamineserver/src/main/java/com/example/anandamineserver/CorsConfig.java
+++ anandamineserver/src/main/java/com/example/anandamineserver/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.example.anandamineserver;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -11,6 +12,7 @@ import java.util.Arrays;
 @Configuration
 public class CorsConfig {
 
+    @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(Arrays.asList("*"));
@@ -20,6 +22,11 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration);
         return source;
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        return new CorsFilter(corsConfigurationSource());
     }
 }
 
