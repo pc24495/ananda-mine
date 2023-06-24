fe2336993c83d2338e7400fa01169bd29add774a
pc24495 <pchummar42@gmail.com>
2023-06-24 13:51:46 -0400

    Fixed environment variable usage and axios querying of env variables

--- client/src/config/axiosConfig.js
+++ client/src/config/axiosConfig.js
@@ -1,9 +1,15 @@
 import axios from "axios";
 
+console.log(
+  "import.meta.env.VITE_REACT_APP_PROD",
+  import.meta.env.VITE_REACT_APP_PROD
+);
+
 const instance = axios.create({
-  baseURL: import.meta.env.REACT_APP_PROD
-    ? "http://18.233.100.220:8080/api"
-    : "http://localhost:5173/api", // Replace with your desired base URL
+  baseURL:
+    import.meta.env.VITE_REACT_APP_PROD === "true"
+      ? "http://18.233.100.220:8080/api"
+      : "http://localhost:5173/api", // Replace with your desired base URL
 });
 
 // const instance = axios.create({ baseURL: "http://localhost:5173/api" });
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,41 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:8080", // Replace with your API endpoint
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+
+// console.log(import.meta);
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  console.log(env);
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        "/api": {
+          target:
+            env.VITE_REACT_APP_PROD === "true"
+              ? "http://18.233.100.220:8080"
+              : "http://localhost:8080", // Replace with your API endpoint
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ""),
+        },
       },
     },
-  },
+  };
 });
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: import.meta.env.VITE_REACT_APP_PROD
+//           ? "http://18.233.100.220:8080"
+//           : "http://localhost:8080", // Replace with your API endpoint
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, ""),
+//       },
+//     },
+//   },
+// });
