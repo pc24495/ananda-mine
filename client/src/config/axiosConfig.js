ceb4904c1385dc3fd660c7ff1f7903d653515778
pc24495 <pchummar42@gmail.com>
2023-06-26 14:07:41 -0400

    Hard coded base url for axios to be anandamide.com for testing purposes

--- client/src/config/axiosConfig.js
+++ client/src/config/axiosConfig.js
@@ -5,11 +5,15 @@ import axios from "axios";
 //   import.meta.env.VITE_REACT_APP_PROD
 // );
 
+// const instance = axios.create({
+//   baseURL:
+//     import.meta.env.VITE_REACT_APP_PROD === "true"
+//       ? "http://www.anandamine.com/api"
+//       : "http://localhost:5173/api", // Replace with your desired base URL
+// });
+
 const instance = axios.create({
-  baseURL:
-    import.meta.env.VITE_REACT_APP_PROD === "true"
-      ? "http://www.anandamine.com/api"
-      : "http://localhost:5173/api", // Replace with your desired base URL
+  baseURL: "http://www.anandamine.com/api",
 });
 
 // const instance = axios.create({ baseURL: "http://localhost:5173/api" });
