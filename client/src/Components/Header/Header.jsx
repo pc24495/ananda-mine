6d2a11112d327bcbc7fa2981d47a6bbd7e1fb81b
pc24495 <pchummar42@gmail.com>
2023-06-27 19:35:45 -0400

    Removed temporary header filler content

--- client/src/Components/Header/Header.jsx
+++ client/src/Components/Header/Header.jsx
@@ -6,8 +6,7 @@ import ForumIcon from "@mui/icons-material/Forum";
 const Header = () => {
   return (
     <div className={classes.Header}>
-      Bla14
-      <PersonIcon fontSize="large" sx={{ color: "#FF00FF" }} />
+      <PersonIcon fontSize="large" sx={{ color: "#424242" }} />
       <LocalFireDepartmentIcon fontSize="large" sx={{ color: "#424242" }} />
       <ForumIcon fontSize="medium" sx={{ color: "#424242" }} />
     </div>
