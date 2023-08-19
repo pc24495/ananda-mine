cb4f9a91da16813f1dee4e2d8ec255c2e04a1e3c
pc24495 <pchummar42@gmail.com>
2023-08-18 22:14:35 -0400

    Added a background image, a header, some text, and a button to the login page

--- client/src/Components/LoginPage/Login.jsx
+++ client/src/Components/LoginPage/Login.jsx
@@ -1,13 +1,27 @@
 // import React from 'react'
 import classes from "./Login.module.scss";
-import MainLoginBox from "./MainLoginBox/MainLoginBox.jsx";
+// import MainLoginBox from "./MainLoginBox/MainLoginBox.jsx";
 
 const Login = () => {
   return (
     <div className={classes.Login}>
-      <MainLoginBox></MainLoginBox>
+      <div className={classes.LoginHeader}></div>
+      <div className={classes.Body}>
+        <div className={classes.InfoBox}>
+          <h1 className={classes.FindYourHeartbeat}>Find Your Heartbeat</h1>
+          <div className={classes.CreateAccountBox}>
+            <div className={classes.CreateAccountButton}>
+              <div className={classes.CreateAccountStyling}>
+                <p>Create Account</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
+
+// <MainLoginBox></MainLoginBox>
 
 export default Login;
