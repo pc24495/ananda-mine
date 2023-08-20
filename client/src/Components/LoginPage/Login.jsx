c046b3354441ea19f3930b241d074b1ee179b3fa
pc24495 <pchummar42@gmail.com>
2023-08-20 16:46:31 -0400

    Added login box, not connected to backend, does not close

--- client/src/Components/LoginPage/Login.jsx
+++ client/src/Components/LoginPage/Login.jsx
@@ -5,6 +5,43 @@ import classes from "./Login.module.scss";
 const Login = () => {
   return (
     <div className={classes.Login}>
+      <div className={classes.LoginBoxOverlay}>
+        <div className={classes.LoginBox}>
+          <p className={classes.CloseButton}>X</p>
+          <img src="/pinkheart.svg" alt="Logo" />
+          <h3 className={classes.CreateAccountHeader}>Create Account</h3>
+          <p className={classes.Terms}>
+            By clicking login, you agree to our <span>terms.</span>
+          </p>
+          <button className={classes.LoginButtonInBox}>
+            Log in with Google (unavailable)
+          </button>
+          <button
+            className={classes.LoginButtonInBox}
+            style={{
+              backgroundColor: "silver",
+              border: "0.125rem solid black",
+              color: "black",
+            }}
+          >
+            Log in with Facebook (unavailable)
+          </button>
+          <button
+            className={classes.LoginButtonInBox}
+            style={{
+              backgroundColor: "silver",
+              border: "0.125rem solid black",
+              color: "black",
+            }}
+          >
+            Log in with Phone Number (unavailable)
+          </button>
+          <p className={classes.LoginInfo}>
+            Click here to learn more about this project. Or watch a video
+            explaining it.
+          </p>
+        </div>
+      </div>
       <div className={classes.LoginHeader}>
         <div className={classes.LoginHeaderLogo}>Anandamine</div>
         <button className={classes.LoginButton}>Log In</button>
