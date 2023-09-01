7fd1ec05e7379cc9aadc905caa19d9402bc991ec
pc24495 <pchummar42@gmail.com>
2023-08-31 20:19:49 -0400

    Setup arrows decrementing/incrementing pic index and tics highlight correspondingly, pics don't change yet

--- client/src/Components/Recs/Card/Card.jsx
+++ client/src/Components/Recs/Card/Card.jsx
@@ -1,7 +1,27 @@
 import classes from "./Card.module.scss";
-import { AiFillInfoCircle } from "react-icons/ai";
+import {
+  AiFillInfoCircle,
+  AiOutlineArrowLeft,
+  AiOutlineArrowRight,
+} from "react-icons/ai";
+import { useState } from "react";
 
 const Card = () => {
+  const [pics] = useState(["1", "2", "3"]);
+  const [currentPicIndex, setCurrentPicIndex] = useState(0);
+
+  const decrementPicIndex = () => {
+    if (currentPicIndex > 0) {
+      setCurrentPicIndex(currentPicIndex - 1);
+    }
+  };
+
+  const incrementPicIndex = () => {
+    if (currentPicIndex < pics.length - 1) {
+      setCurrentPicIndex(currentPicIndex + 1);
+    }
+  };
+
   return (
     <div className={classes.Card}>
       <div className={classes.CardMain}>
@@ -12,8 +32,29 @@ const Card = () => {
         />
       </div>
       <div className={classes.CardOverlay}>
-        <div className={classes.SlideTicks}></div>
-        <div className={classes.ArrowBox}></div>
+        <div className={classes.SlideTicks}>
+          {pics.map((pic, index) => {
+            return (
+              <div
+                key={"tick_" + index}
+                className={classes.Tick}
+                style={{
+                  backgroundColor: index == currentPicIndex ? "white" : "gray",
+                }}
+              ></div>
+            );
+          })}
+        </div>
+        <div className={classes.ArrowBox}>
+          <div className={classes.LeftArrowBox} onClick={decrementPicIndex}>
+            <AiOutlineArrowLeft className={classes.Arrow}></AiOutlineArrowLeft>
+          </div>
+          <div className={classes.RightArrowBox} onClick={incrementPicIndex}>
+            <AiOutlineArrowRight
+              className={classes.Arrow}
+            ></AiOutlineArrowRight>
+          </div>
+        </div>
         <div className={classes.InfoBox}>
           <div className={classes.MainInfoBox}>
             <div className={classes.NameAndAge}>
