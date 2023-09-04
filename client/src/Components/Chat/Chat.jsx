1ba042bac40cb20eef0822f58a4f0e14c47a18f6
pc24495 <pchummar42@gmail.com>
2023-09-04 19:34:10 -0400

    Chat box properly styled, no backend integration yet, no profile view

--- client/src/Components/Chat/Chat.jsx
+++ client/src/Components/Chat/Chat.jsx
@@ -1,16 +1,60 @@
 // import PropTypes from 'prop-types'
 import classes from "./Chat.module.scss";
-import { useNavigate } from "react-router-dom";
+import { AiOutlineCloseCircle } from "react-icons/ai";
+// import { useNavigate } from "react-router-dom";
 
 const Chat = () => {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
   return (
     <div className={classes.Chat}>
-      <div className={classes.ChatBox}></div>
+      <div className={classes.ChatBox}>
+        <div className={classes.ChatHeader}>
+          <div className={classes.ProfilePicContainer}>
+            <img
+              src="/TomCruiseProfilePic.webp"
+              alt="Profile Pic"
+              className={classes.ProfilePic}
+            />
+          </div>
+          <p className={classes.MatchInfo}>
+            You matched with Tom on 4:18PM 8/31/23
+          </p>
+          <AiOutlineCloseCircle
+            className={classes.CloseButton}
+          ></AiOutlineCloseCircle>
+        </div>
+        <div className={classes.ChatMain}>
+          <div className={classes.UserMessageContainer}>
+            <div className={classes.UserMessage}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            </div>
+          </div>
+          <div className={classes.UserMessageContainer}>
+            <div className={classes.UserMessage}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            </div>
+          </div>
+          <div className={classes.MatchMessageContainer}>
+            <div className={classes.MatchMessage}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            </div>
+          </div>
+        </div>
+        <form className={classes.ChatForm}>
+          <textarea
+            className={classes.ChatInput}
+            placeholder="Type a message"
+          ></textarea>
+          <button className={classes.SubmitButton}>Send</button>
+        </form>
+      </div>
       <div className={classes.Profile}></div>
     </div>
   );
 };
+
+// <div className={classes.ChatBody}></div>
+//         <form className={classes.ChatForm}></form>
 
 // Chat.propTypes = {}
 
